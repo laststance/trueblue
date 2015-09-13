fbdbafe2bf4507040bcb82a4c41fc3fce31d8bc3
ryota-murakami <dojce1048@gmail.com>
2015-09-13 11:05:36 +0900

    [gulpfile.js] fix attempt use 'base' option

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,10 @@ var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
 
 gulp.task('sass', function() {
-  return sass('app/Resources/scss/*.scss', {style: 'expanded', base: 'app'})
+  return sass('./app/Resources/scss/*.scss', {style: 'expanded'})
   .pipe(gulp.dest('./web/css'));
 });
 
 gulp.task('watch', function() {
-  gulp.watch('app/Resources/scss/*.scss', ['sass']);
+  gulp.watch('./app/Resources/scss/*.scss', ['sass']);
 });
