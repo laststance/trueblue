a3391cbf323f21423a26a144dfcb64be8af7f1e2
ryota-murakami <dojce1048@gmail.com>
2015-09-14 22:56:14 +0900

    comment out automaticaly generated test

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -8,11 +8,11 @@ class DefaultControllerTest extends WebTestCase
 {
     public function testIndex()
     {
-        $client = static::createClient();
+        //$client = static::createClient();
 
-        $crawler = $client->request('GET', '/app/example');
+        //$crawler = $client->request('GET', '/app/example');
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertTrue($crawler->filter('html:contains("Homepage")')->count() > 0);
+        //$this->assertEquals(200, $client->getResponse()->getStatusCode());
+        //$this->assertTrue($crawler->filter('html:contains("Homepage")')->count() > 0);
     }
 }
