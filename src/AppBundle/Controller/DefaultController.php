716ce8f39e24b123175e0aa7aab4517571e8a57e
ryota-murakami <dojce1048@gmail.com>
2015-09-17 00:03:50 +0900

    clean dump code

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,7 +14,7 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         $twitter_api = $this->container->get('twitter_api');
-        dump(get_class($this->get('security.token_storage')->getToken()));
+
         return $this->render('AppBundle:Default:index.html.twig');
     }
 }
