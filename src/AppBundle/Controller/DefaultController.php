7a1a17954851587b50c179dd8c33b331634faab2
ryota-murakami <dojce1048@gmail.com>
2015-09-20 15:26:59 +0900

    add BearerToken parameter

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,7 +14,11 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         $twitter_api = $this->container->get('twitter_api');
-
-        return $this->render('AppBundle:Default:index.html.twig');
+        // dump($this->get('security.token_storage')->getToken());
+        dump($twitter_api->getBearerToken());
+        //今日のつぶやき一覧を取得
+        $today_tweet = '';
+        //今日のつぶやき一覧をtemplateに貼り付けてrender
+        return $this->render('AppBundle:Default:index.html.twig', array('today_tweet' => $today_tweet));
     }
 }
--- src/AppBundle/Service/TwitterAPI.php
+++ src/AppBundle/Service/TwitterAPI.php
@@ -13,8 +13,8 @@ class TwitterAPI
      */
     protected $oauth_token;
 
-    protected $consumer_key = '';
-    protected $consumer_secret = '';
+    protected $consumer_key = ''; // api key
+    protected $consumer_secret = ''; // api secret
 
     /**
      * @param TokenStorage $token_storage $this->container->get('security.token_storage')
@@ -29,5 +29,41 @@ class TwitterAPI
         $this->oauth_token = $oauth_token;
         $this->consumer_key = $key_and_token['consumer_key'];
         $this->consumer_secret = $key_and_token['consumer_secret'];
+    }
+
+    /**
+     * @return string BearerToken
+     */
+    public function getBearerToken()
+    {
+        $api_key = $this->consumer_key;
+        $api_secret = $this->consumer_secret;
+
+        // クレデンシャルを作成
+        $credential = base64_encode($api_key . ':' . $api_secret);
+
+        // リクエストURL
+        $request_url = 'https://api.twitter.com/oauth2/token';
+
+        // リクエスト用のコンテキストを作成する
+        $context = array(
+          'http' => array(
+            'method' => 'POST',
+            'header' => array(
+              'Authorization: Basic ' . $credential,
+              'Content-Type: application/x-www-form-urlencoded;charset=UTF-8' ,
+            ),
+            'content' => http_build_query(array( 'grant_type' => 'client_credentials')),
+          ),
+        );
+
+        $response_json = @file_get_contents($request_url, false, stream_context_create($context));
+        $decoded_json = json_decode($response_json);
+
+        if ($decoded_json->token_type !== 'bearer') {
+            throw new \Exeption('faild to get the BearerToken');
+        }
+
+        return $decoded_json->access_token;
     }
 }
