be580f98049834185b2993867f8aaf2ca4ee7666
ryota-murakami <dojce1048@gmail.com>
2015-10-02 00:31:14 +0900

    [CronCommand] cron:SaveTargetDateTimeline コマンドを作成

--- src/AppBundle/Command/CronCommand.php
+++ src/AppBundle/Command/CronCommand.php
@@ -9,27 +9,63 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use AppBundle\Service\TwitterAPI;
 use AppBundle\Entity\User;
+use AppBundle\Entity\PastTimeline;
 
+/**
+ * 指定した日付の全ユーザーのタイムラインをDBに保存する
+ *
+ * app/sonsole cron:SaveTargetDateTimeline yyyy-mm-dd
+ */
 class CronCommand extends ContainerAwareCommand
 {
     protected function configure()
     {
-        $this->setName('cron:SaveYesterdayTimeline');
+        $this->setName('cron:SaveTargetDateTimeline')
+          ->setDescription('Save to DB All users Target Date Timeline.')
+          ->addArgument(
+            'date',
+            InputArgument::REQUIRED,
+            'what date do you want to save?'
+          );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        var_dump($this->getContainer()->get('security.token_storage'));
-        exit();
-        $doctrine = $this->getContainer()->get('doctrine');
-        //$user =
-        $twitterApi = new TwitterAPI($doctrine);
+        // 引数のフォーマットバリデーション
+        if (!preg_match('/[0-9]{4}-[0-9]{2}-[0-9]{2}/', $arg_date = $input->getArgument('date'))) {
+            throw new \Exeption('invalid argument. date format must be yyyy-mm-dd. e.g. 2020-04-03');
+        }
 
+        $doctrine = $this->getContainer()->get('doctrine');
+        $em = $doctrine->getEntityManager();
         // 登録ユーザーを取得
         $users = $doctrine->getRepository('AppBundle:User')->findAll();
-        print_r($users);
-        // 昨日のタイムラインを取得
+        // tiwtter_apiのアクセストークン類
+        $api_parameter = $this->getContainer()->getParameter('twitter_api');
+        // タイムラインを取得する日付
+        $targetDate = new \DateTime($arg_date);
 
-        //$output->writeln($text);
+        // メイン処理
+        foreach ($users as $user) {
+            // 昨日のタイムラインJsonを取得
+            $twitterApi = new twitterApi($doctrine, $user, $api_parameter);
+            $timeline_json = $twitterApi->findIdRangeByDate($targetDate)['timeline_json'];
+            $encoded_json = json_encode($timeline_json); // DBにはJSON文字列をまるごと格納する
+
+            // DBに保存するTimeLineオブジェクトを作成
+            $pastTimeLine = new PastTimeline();
+            $pastTimeLine->setUser($user);
+            $pastTimeLine->setTimelineJson($encoded_json);
+            $pastTimeLine->setDate($targetDate);
+            $now = new \DateTime();
+            $pastTimeLine->setCreateAt($now);
+            $pastTimeLine->setUpdateAt($now);
+
+            // DB保存処理
+            $em->persist($pastTimeLine);
+            $em->flush();
+        }
+
+        $output->writeln('Save PastTimeLine Complete.');
     }
 }
