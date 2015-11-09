180ae731b5d14d87c5046526c1caa548cdc76bbd
ryota-murakami <dojce1048@gmail.com>
2015-11-09 21:21:45 +0900

    Revert "PHPUnitのコントローラテストを削除"
    This reverts commit 6e9e9eac957ed3901fc6ca6f8d49dc1ade9eb0a4.

---
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        //$client = static::createClient();
+
+        //$crawler = $client->request('GET', '/app/example');
+
+        //$this->assertEquals(200, $client->getResponse()->getStatusCode());
+        //$this->assertTrue($crawler->filter('html:contains("Homepage")')->count() > 0);
+    }
+
+    public function testLogin()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/login');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        $this->assertTrue($crawler->filter('body.login')->count() > 0);
+    }
+}
