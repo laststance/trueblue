290303c52a5d743d6bd531c653f2d403ce8a354a
ryota-murakami <dojce1048@gmail.com>
2016-10-29 23:35:48 +0900

    install react-bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -13,11 +13,11 @@ class AppKernel extends Kernel
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
+            new Limenius\ReactBundle\LimeniusReactBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
