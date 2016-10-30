df430d299de71bd85f43ad1991e653547ef979db
ryota-murakami <dojce1048@gmail.com>
2016-10-30 20:00:40 +0900

    [.gitignore] add web/assets/

--- client/Recipes-redux/startup/clientRegistration.jsx
+++
@@ -1,8 +0,0 @@
-import ReactOnRails from 'react-on-rails';
-import RecipesApp from './RecipesAppClient';
-import configureStore from '../store/recipesStore';
-
-var recipesStore = configureStore;
-
-ReactOnRails.registerStore({recipesStore})
-ReactOnRails.register({ RecipesApp });
