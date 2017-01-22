4572e3069a9398fd2bbff7474a28e0fe1abf86b2
ryota-murakami <dojce1048@gmail.com>
2017-01-23 00:09:32 +0900

    refactor js test

--- test/header-spec.js
+++ test/header-spec.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import ReactTestUtils from 'react-addons-test-utils'
-
-import assert from 'assert';
-import Header from '../app/Resources/js/components/header.jsx';
+import assert from 'assert'
+import Header from '../app/Resources/js/components/header.jsx'
 
 describe('testHeader', () => {
+    const getComponent = () => {
+        return ReactTestUtils.renderIntoDocument(<Header getDailyJson={[]} timelineDateList={[]} appUsername={'hoge'}/>)
+    }
+
     it('username', () => {
-        let component = ReactTestUtils.renderIntoDocument(<Header getDailyJson={[]} timelineDateList={[]} appUsername={'hoge'}/>)
+        let component = getComponent()
         assert(component.props.appUsername == 'hoge')
-    });
+    })
     it('get_kaomoji', () => {
-        let component = ReactTestUtils.renderIntoDocument(<Header getDailyJson={[]} timelineDateList={[]} appUsername={'hoge'}/>)
+        let component = getComponent()
         assert(typeof component.get_kaomoji() == 'string')
-    });
-});
+    })
+})
