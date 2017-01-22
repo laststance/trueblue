88ada770b98ab21c494e23359bc5e7f2751dec79
ryota-murakami <dojce1048@gmail.com>
2017-01-22 23:28:38 +0900

    refactor

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,37 +14,14 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $twitterApi = $this->container->get('twitter_api');
-        $twitterApi->setUser($this->getUser());
-
-        // 今日のtimelineを取得
-        $timeline = $twitterApi->getTodayTimeline();
-        // TODO:
-        if (!isset($timeline['error'])) {
-            $timeline = $this->get('app.service.common_service')->enableHtmlLink($timeline);
-        }
-        // DBから過去のtimelinelistを取得
-        $pastTimelines = $this->getDoctrine()->getRepository('AppBundle:PastTimeline')->findByUser(
-            $this->getUser(),
-            ['date' => 'DESC']
-        );
-        $timelineDateList = array_map(
-            function ($obj) {
-                return $obj->getDate()->format('Y-m-d');
-            },
-            $pastTimelines
-        );
-        // 今日のタイムライン表示ボタンに使用
-        array_unshift($timelineDateList, (new \DateTime())->format('Y-m-d'));
-
         return $this->render(
             ':default:index.html.twig',
             [
                 'props' => $this->get('jms_serializer')->serialize(
                     [
                         'json_daily_url' => $this->generateUrl('json_daily'),
-                        'timeline_date_list' => $timelineDateList,
-                        'timeline_json' => $timeline,
+                        'timeline_date_list' => $this->fetchPastTimelineDate(),
+                        'timeline_json' => $this->fetchTodayTimeline(),
                         'app_user_username' => $this->getUser()->getUsername(),
                     ],
                     'json'
@@ -59,5 +36,39 @@ class DefaultController extends Controller
     public function loginAction()
     {
         return $this->render(':default:login.html.twig');
+    }
+
+    private function fetchTodayTimeline(): array
+    {
+        $twitterApi = $this->container->get('twitter_api');
+        $twitterApi->setUser($this->getUser());
+
+        $timeline = $twitterApi->getTodayTimeline();
+        if (!isset($timeline['error'])) {
+            $timeline = $this->get('app.service.common_service')->enableHtmlLink($timeline);
+        }
+
+        return $timeline;
+    }
+
+    /**
+     * @return array
+     */
+    private function fetchPastTimelineDate()
+    {
+        $pastTimelines = $this->getDoctrine()->getRepository('AppBundle:PastTimeline')->findByUser(
+            $this->getUser(),
+            ['date' => 'DESC']
+        );
+        $timelineDateList = array_map(
+            function ($obj) {
+                return $obj->getDate()->format('Y-m-d');
+            },
+            $pastTimelines
+        );
+        // 今日のタイムライン表示ボタンに使用
+        array_unshift($timelineDateList, (new \DateTime())->format('Y-m-d'));
+
+        return $timelineDateList;
     }
 }
