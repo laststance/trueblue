93652660b888dea378a47638780da3497ab7a7f3
ryota-murakami <dojce1048@gmail.com>
2017-01-24 21:15:25 +0900

    Revert "travis fail karma fix"
    This reverts commit 603eb5ee8bba3c881cee80d4e89a9faa82449c24.

--- test/header-spec.js
+++ test/header-spec.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactTestUtils from 'react-addons-test-utils'
 import assert from 'assert'
-import {getKaomoji, getYmdStr } from '../app/Resources/js/utils/util.js'
 import Header from '../app/Resources/js/components/header.jsx'
 
 describe('testHeader', () => {
