405ff520761f3978c3a9ab0a75f3758927230e0f
ryota-murakami <dojce1048@gmail.com>
2017-02-09 22:02:29 +0900

    card

--- app/Resources/js/components/timeline.jsx
+++ app/Resources/js/components/timeline.jsx
@@ -31,11 +31,17 @@ export default class Timeline extends React.Component {
                                         <img className="profile-image" src={tweet.user.profile_image_url}/>
                                     </div>
                                     <div className="right-side">
-                                        <span className="user-name">{tweet.user.name}</span> <span
-                                        className="screen-name">@{tweet.user.screen_name}</span>
+                                        <span className="user-name">{tweet.user.name}</span>
+                                        <span className="screen-name">@{tweet.user.screen_name}</span>
                                         <p className="text" dangerouslySetInnerHTML={{__html: tweet.text}}></p>
-                                        <p
-                                            className="create-at">{date.getFullYear()}年{date.getMonth() + 1}月{date.getDate()}日 {date.getHours()}時{date.getMinutes()}分</p>
+                                        {( () => {
+                                            let urls = tweet.entities.urls
+                                            if(urls.length > 0) {
+                                                const url = 'https://hatenablog-parts.com/embed?url=' + urls[urls.length-1].url
+                                                return <p style={{margin:'10px 0'}}><iframe src={url} className="urlcard" scrolling="no" frameBorder="0" style={{width:'100%', height:'155px', maxWidth:'500px'}}></iframe></p>
+                                            }
+                                        })()}
+                                        <p className="create-at">{date.getFullYear()}年{date.getMonth() + 1}月{date.getDate()}日 {date.getHours()}時{date.getMinutes()}分</p>
                                     </div>
                                 </div>
                             </section>
