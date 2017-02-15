80aea93e18490e51cb1e9b0a2ad6fcd60d2e0c32
ryota-murakami <dojce1048@gmail.com>
2017-02-15 22:35:14 +0900

    add trans

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -16,18 +16,27 @@ class DefaultControllerTest extends MyControllerTestCase
 
     public function testIndex()
     {
-        $this->client = self::createClient();
-
         // not login
+        $this->client = self::createClient();
         $this->client->request('GET', '/');
         $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
         $this->assertContains('Login with Twitter', $this->client->getResponse()->getContent());
 
         // login
+        $this->client = self::createClient();
         $this->logIn();
         $this->client->request('GET', '/');
         $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
         $this->assertContains('Home', $this->client->getResponse()->getContent());
+
+        // transtation
+        $this->client = self::createClient();
+        $this->client->request('GET', '/', [], [], ['HTTP_ACCEPT_LANGUAGE' => 'ja,en-US;q=0.8,en;q=0.6']);
+        $this->assertContains('Daily Tweetはtwitter上の自分のつぶやきを日別にまとめるサービスです。', $this->client->getResponse()->getContent());
+        $this->client->request('GET', '/', [], [], ['HTTP_ACCEPT_LANGUAGE' => 'ja,en-US;q=0.8,en;q=0.6']);
+
+        $this->client->request('GET', '/', [], [], ['HTTP_ACCEPT_LANGUAGE' => 'en-US,en;q=0.8,ja;q=0.6']);
+        $this->assertContains('Daily Tweet is archive tweets on every other day.', $this->client->getResponse()->getContent());
     }
 
     public function testHome()
