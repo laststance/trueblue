6b387f8fe1deb3f3087b4bf9dc765fd44c402f63
ryota-murakami <dojce1048@gmail.com>
2017-06-09 16:19:09 +0900

    fix https://github.com/webpack/webpack-dev-server/issues/533

--- webpack.config.js
+++ webpack.config.js
@@ -85,7 +85,12 @@ if (devBuild) {
     config.devServer = {
         hot: true,
         contentBase: path.resolve('./web/'),
-        inline: true
+        inline: true,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+            'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+        }
     }
     config.plugins.push(
         new webpack.HotModuleReplacementPlugin()
