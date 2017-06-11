d6538fa993f0e936599fd8d4361bfe3949af07a0
ryota-murakami <dojce1048@gmail.com>
2017-06-11 12:51:04 +0900

    fix irregular array index

--- src/AppBundle/Controller/MainController.php
+++ src/AppBundle/Controller/MainController.php
@@ -49,6 +49,8 @@ class MainController extends Controller
     private function fetchTimeline(User $user): array
     {
         $res = [];
+        $res[$this->get('app.service.common_service')->getToday()] = $this->fetchTodayTimeline($user);
+
         $repository = $this->get('doctrine.orm.default_entity_manager')->getRepository('AppBundle:PastTimeline');
         $pastTimelines = $repository->findBy(
             [
@@ -65,7 +67,6 @@ class MainController extends Controller
                 $res[$item->getDate()->format('Y-m-d')] = $item->getTimeline();
             }
         }
-        $res[$this->get('app.service.common_service')->getToday()] = $this->fetchTodayTimeline($user);
 
         return $res;
     }
