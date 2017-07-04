102236082aa2a9e8917773f6531238a11f71f0dc
ryota-murakami <dojce1048@gmail.com>
2017-07-04 22:37:51 +0900

    re:re :green_apple:

--- app/Resources/js/reducers/home.js
+++ app/Resources/js/reducers/home.js
@@ -1,7 +1,7 @@
 import homeReducer from './homeReducer'
 import { initialState as homeState } from './homeReducer'
 import { combineReducers }  from 'redux'
-
+// TODO Require file?
 export default combineReducers({
     homeState: homeReducer
     
