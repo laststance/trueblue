9a4d8a82bafc60c8624067c6f4574fb39f60e4be
ryota-murakami <dojce1048@gmail.com>
2017-07-05 03:05:55 +0900

    re:re :green_apple:

--- app/Resources/js/actions/home.js
+++ app/Resources/js/actions/home.js
@@ -37,7 +37,7 @@ const Actions = {
             })
         }
     },
-    setCurrentDate: (ymdString, currentIndexNumber) => {
+    setCurrentDate: (ymdString) => {
         return dispatch => {
             dispatch({
                 type: Constants.SET_CURRENT_DATE_AND_INDEX,
