5e8ef3591bad90bafcc5c2904b8a5cc56938ca0d
ryota-murakami <dojce1048@gmail.com>
2017-07-06 15:11:26 +0900

    re:re :green_apple:

--- jest/reducer.test.js
+++ jest/reducer.test.js
@@ -2,7 +2,7 @@ import homeReducer from '../app/Resources/js/reducers/homeReducer'
 import { getYmdStr } from '../app/Resources/js/utils/util'
 
 describe('homeReducer', () => {
-    it('give undefined action type, return initial state', () => {
+    it('not give action type, return initial state', () => {
         expect(homeReducer(undefined, {})).toEqual(
             {
                 fetching: false,
