9f35d4bb96430bf2b7f7e5d139ab5d4b90f89933
ryota-murakami <dojce1048@gmail.com>
2017-07-06 15:21:23 +0900

    re:re :green_apple:

--- jest/reducer.test.js
+++ jest/reducer.test.js
@@ -1,15 +1,27 @@
 import homeReducer from '../app/Resources/js/reducers/homeReducer'
+import Constants from '../app/Resources/js/constants/home'
 import { getYmdStr } from '../app/Resources/js/utils/util'
+
+const initialState = {
+    fetching: false,
+    timelineJson: {},
+    username: '',
+    currentDate: getYmdStr(new Date())
+}
 
 describe('homeReducer', () => {
     it('not give action type, return initial state', () => {
         expect(homeReducer(undefined, {})).toEqual(
-            {
-                fetching: false,
-                timelineJson: {},
-                username: '',
-                currentDate: getYmdStr(new Date())
-            }
+            initialState
+        )
+    })
+    it('give AJAX_FETCH_START', () => {
+        initialState.fetching = true
+
+        expect(homeReducer(undefined, {
+            type: Constants.AJAX_FETCH_START
+        })).toEqual(
+            initialState
         )
     })
 })
