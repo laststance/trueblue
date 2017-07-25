389659845a78e87d58352a7d19858c4a6ae090c2
ryota-murakami <dojce1048@gmail.com>
2017-07-26 01:31:32 +0900

    add prevFetch test

---
+++ jest/action.test.js
@@ -0,0 +1,34 @@
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import Actions from '../app/Resources/js/actions/home'
+import Types from '../app/Resources/js/constants/home'
+
+const middlewares = [thunk]
+const mockStore = configureMockStore(middlewares)
+
+global.fetch = jest.fn().mockImplementation(() => {
+    return new Promise((resolve, reject) => {
+        resolve({
+            ok: true,
+            Id: '123',
+            json: function () {
+                return {timelineJson: {key: 'value'}}
+            }
+        })
+    })
+})
+
+describe('fetchSingleDate()', () => {
+    it('test dispatch() value', () => {
+        const username = 'foo'
+        const date = '2017-03-30'
+        const store = mockStore()
+
+        return store.dispatch(Actions.fetchSingleDate(username, date))
+            .then(() => {
+                const [prevFetch, afterFetch] = store.getActions()
+
+                expect(prevFetch.type).toEqual(Types.AJAX_FETCH_START)
+            })
+    })
+})
